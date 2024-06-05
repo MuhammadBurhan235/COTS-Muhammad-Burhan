0b27712925443dd81f93d0f87a345586c48be860
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2024-06-05 11:31:47 +0700

    Update App.java
    deploy

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String pesan = "Hello World!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(pesan);
+        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
-        return pesan;
+        return MESSAGE;
     }
 }
