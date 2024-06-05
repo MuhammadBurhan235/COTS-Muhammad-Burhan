4f7800bb25374003f74d132018f200616a31a8d2
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2024-06-05 11:25:10 +0700

    Update App.java
    pesan

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String pesan = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(pesan);
     }
 
     public String getMessage() {
-        return MESSAGE;
+        return pesan;
     }
 }
